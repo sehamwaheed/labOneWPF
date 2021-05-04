96389aa301e66a10d16ca12cff0ec0b575b211cc
dina waheed <dinawaheed3@gmail.com>
2021-05-05 01:59:28 +0200

    Add project files.

---
+++ labOneWPF/MainWindow.xaml.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace labOneWPF
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void okSaved(object sender, RoutedEventArgs e)
+        {
+            string message = $"FirstName:{fname.Text}\nLastName:{lname.Text}\nGender:{Gender.Text}\nAddress:{Address.Text}\nPhone:{Phone.Text}\nMobile:{Mobile.Text}\nEmail:{Email.Text}\nJobTitele:{JobTitel.Text}\n";
+          
+            string caption = "Confirmation";
+            MessageBoxButton buttons = MessageBoxButton.OKCancel;
+            MessageBoxImage icon = MessageBoxImage.Question;
+            if (MessageBox.Show(message, caption, buttons, icon) == MessageBoxResult.OK)
+            {
+                // OK code here  
+                MessageBox.Show("saved successfully");
+            }
+            else
+            {
+                fname.Text = lname.Text = Gender.Text = Address.Text = Phone.Text = Mobile.Text = Email.Text = JobTitel.Text = string.Empty;
+            }
+        }
+
+        private void cancelEvent(object sender, RoutedEventArgs e)
+        {
+            fname.Text = lname.Text = Gender.Text = Address.Text = Phone.Text = Mobile.Text = Email.Text = JobTitel.Text = string.Empty;
+        }
+    }
+}
---
+++ labOneWPF/taskThree.xaml.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace labOneWPF
+{
+    /// <summary>
+    /// Interaction logic for taskThree.xaml
+    /// </summary>
+    public partial class taskThree : Window
+    {
+        public taskThree()
+        {
+            InitializeComponent();
+            content.Text = "Starting Default Text";
+        }
+
+        private void setText(object sender, RoutedEventArgs e)
+        {
+            content.Text = "Starting Default Text Set Text";
+        }
+
+        private void selectAll(object sender, RoutedEventArgs e)
+        {
+            content.Focus();
+            content.SelectAll();
+        }
+
+        private void Clear(object sender, RoutedEventArgs e)
+        {
+            content.Text = string.Empty;
+        }
+
+        private void Prepand(object sender, RoutedEventArgs e)
+        {
+          content.Text= $" 'Prepand Text '  {content.Text}";
+        }
+
+        private void Insert(object sender, RoutedEventArgs e)
+        {
+            if (content.CaretIndex == 0)
+            {
+                content.SelectionStart = content.Text.Length;
+                content.Text = $"{content.Text}  ** inserted text ** ";
+            }
+            else
+            {
+                content.Text = content.Text.Insert(content.CaretIndex, "** inserted text **");
+            }
+
+        }
+
+        private void Append(object sender, RoutedEventArgs e)
+        {
+            content.Text = $"   {content.Text}  'Append Text '";
+        }
+
+        private void Cut(object sender, RoutedEventArgs e)
+        {
+            content.Focus();
+            content.Cut();
+        }
+
+        private void Past(object sender, RoutedEventArgs e)
+        {
+            content.Focus();
+            content.Paste();
+        }
+
+        private void Undo(object sender, RoutedEventArgs e)
+        {
+            content.Focus();
+            content.Undo();
+        }
+
+        private void edit(object sender, RoutedEventArgs e)
+        {
+            content.IsReadOnly = false;
+        }
+
+        private void readOnly(object sender, RoutedEventArgs e)
+        {
+            content.IsReadOnly = true;
+        }
+
+        private void leftAline(object sender, RoutedEventArgs e)
+        {
+            content.TextAlignment = TextAlignment.Left;
+        }
+
+        private void centerAline(object sender, RoutedEventArgs e)
+        {
+            content.TextAlignment = TextAlignment.Center;
+        }
+
+        private void rightAline(object sender, RoutedEventArgs e)
+        {
+            content.TextAlignment = TextAlignment.Right;
+        }
+    }
+}
